48377f38396171e0be68f7206a5df29a8e3b8280
xzwb <1478863479@qq.com>
2020-01-26 17:10:05 +0800

    反射调用方大

--- src/com/xzwb/fanshe/ReflectTest.java
+++ src/com/xzwb/fanshe/ReflectTest.java
@@ -3,6 +3,7 @@ package com.xzwb.fanshe;
 import com.xzwb.info.User;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 
 /**
  * 使用反射创建对象
@@ -16,6 +17,10 @@ public class ReflectTest {
             // 调用有参构造器
             Constructor<User> constructor = c.getDeclaredConstructor(int.class, String.class);
             User user = constructor.newInstance(101, "yyf");
+            System.out.println(user);
+            // 通过反射API调用普通方法
+            Method method = c.getDeclaredMethod("setName", String.class);
+            method.invoke(user, "xzwb");
             System.out.println(user);
         } catch (Exception e) {
             e.printStackTrace();
